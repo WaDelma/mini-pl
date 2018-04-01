34888bbdd37781e40993d64ff12d45677fe743d8
Delma <delma@del.ma>
2018-04-01 18:56:41 +0300

    Fixed test.

--- mini-pl/src/parser/ast.rs
+++ mini-pl/src/parser/ast.rs
@@ -71,6 +71,7 @@ pub enum StmtError {
     /// Invalid assignment operator
     InvalidAssignment,
     /// Semicolon missing at the end of statement
+    // TODO: Remove statement from this and handle other errors overlapping with missing semicolon one better.
     MissingSemicolon(Box<Stmt>),
 }
 
